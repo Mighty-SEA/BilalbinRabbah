34b400a2fa0cfe2c9d2cc067bd919022724b3d9d
RyanDixon21 <febryanyusufrp@gmail.com>
2024-10-19 17:25:18 +0700

    tes

--- database/migrations/2024_10_19_094727_guru.php
+++ database/migrations/2024_10_19_094727_guru.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('guru', function(Blueprint $table){
             $table -> id();
             $table -> string('nama');
+            $table -> bigInteger('nik');
             $table -> timestamps();
         });
     }
