05bc403a0d0426e51093cce68ac85a0a67821c0d
RyanDixon21 <febryanyusufrp@gmail.com>
2024-11-01 17:18:45 +0700

    tes

--- app/Models/spp.php
+++ app/Models/spp.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class spp extends Model
+class Spp extends Model
 {
-    protected $table='spp';
-    protected $guarded=[];
-
     use HasFactory;
+    protected $table = 'spp';
+    protected $guarded = [];
 
-    public function murid(){
-        return $this->belongsTo(murid::class, 'nis', 'nis');
+
+    public function Murid(){
+        return $this->belongsTo(Murid::class , 'nis' , 'nis');
     }
 }
